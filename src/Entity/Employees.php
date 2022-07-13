49ae292281281f622c92671177f80c30f2b40b55
KvRae <karamelmannai@gmail.com>
2022-07-13 11:03:24 +0100

    Intialising project

---
+++ src/Entity/Employees.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Employees
+ *
+ * @ORM\Table(name="employees", uniqueConstraints={@ORM\UniqueConstraint(name="matricule", columns={"matricule"})})
+ * @ORM\Entity
+ */
+class Employees
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="matricule", type="integer", nullable=false)
+     */
+    private $matricule;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_prenom", type="string", length=255, nullable=false)
+     */
+    private $nomPrenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="plan", type="text", length=65535, nullable=false)
+     */
+    private $plan;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="travaux faits", type="string", length=255, nullable=false)
+     */
+    private $travauxFaits;
+
+
+}
